60d228c2b88f1430a7021baeaada28a613497732
Max <maxfurman1@gmail.com>
2017-07-24 19:10:31 -0400

    Everything works but Update

--- app/products_app.py
+++ app/products_app.py
@@ -73,34 +73,47 @@ def create_products():
 def update_products():
     print ("Updating a Product")
     updated_product_id = input ("Please input the ID of the product you'd like to update:")
-    with open(csv_file_path, "r") as csv_file:
-        reader = csv.DictReader(csv_file)
-        if updated_product_id == row["id"]:
-            return updated_product_id
-            updated_product_name = input ("Please change name from " , + products["name"], + " to:")
-            updated_product_aisle = input ("Please change aisle from " , + products["aisle"], + " to:")
-            updated_product_department = input ("Please change department from " , + products["department"], + " to:")
-            updated_product_price = input ("Please change name price " , + products["price"], + " to:")
-            replaced_product = {
-                "id": updated_product_id,
-                "name": updated_product_name,
-                "aisle":  updated_product_aisle,
-                "department": updated_product_department,
-                "price": updated_product_price
+    product = [p for p in products if p["id"]==updated_product_id]
+    if product:
+        product = product[0]
+        print ("Please input some information below: ")
+        updated_product_name = input ("Please change name from " + product["name"] + " to:")
+        updated_product_aisle = input ("Please change aisle from " + product["aisle"] + " to:")
+        updated_product_department = input ("Please change department from " + product["department"] + " to:")
+        updated_product_price = input ("Please change price from " + product["price"] + " to:")
+        replaced_product = {
+            "id": updated_product_id,
+            "name": updated_product_name,
+            "aisle":  updated_product_aisle,
+            "department": updated_product_department,
+            "price": updated_product_price
                 }
-            print ("Updated product is ", replaced_product)
-            with open(csv_file_path, "w") as csv_file:
-                writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
-                writer.writeheader() # uses fieldnames set above
-                for product in products if updated_product_id == row["id"]:
-                    writer.writerow(product)
-                print ("\n")
-                print ("New product updated. Good-bye!")
-        else:
-            print ("Unrecognized ID. Please try again.")
+        print ("Updated product is ", replaced_product)
+        with open(csv_file_path, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+            writer.writeheader() # uses fieldnames set above
+            if updated_product_id == product["id"]:
+                writer.writerow(replaced_product)
+            print ("\n")
+            print ("New product updated. Good-bye!")
+    else:
+        print ("Unrecognized ID. Please try again.")
 
 def destroy_products():
     print ("Destroying a Product")
+    destroy_product = input("Please specify the product's identifier: ")
+    product = [p for p in products if p["id"] == destroy_product]
+    if product:
+        product = product[0]
+        print("Product " + product["id"] + ": " + product["name"] + " is being destroyed.")
+        del products[products.index(product)]
+        with open(csv_file_path, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+            writer.writeheader()
+            for product in products:
+                writer.writerow(product)
+    else:
+        print("Try again")
 
 while True:
     if chosen_operation == "List":
